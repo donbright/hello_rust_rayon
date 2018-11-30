406a7e436dbc257905b7e47034b832b18a5087ad
don bright <hmbright@fastmail.com>
2018-11-30 02:17:59 -0600

    update iteration style

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,8 @@ fn process_using_multithreads(input: &mut [i32]) {
 
 fn process_using_singlethread(input: &mut [i32]) {
     for i in 1..1000 {
-      for p in 0..input.len() {
-        input[p] = input[p].wrapping_add(input[p] - input[p]/i);
-      }
+      input.iter_mut()
+           .for_each(|p| *p = (*p).wrapping_add( *p - *p/i ) );
    }
 }
 
@@ -32,7 +31,7 @@ fn main() {
     let mut myvec1 = Vec::new();
     let mut myvec2 = Vec::new();
     println!("building simple test vectors...\n");
-    for i in 0..1024*100 {
+    for i in 0..1024*1280 {
       myvec1.push(i);
       myvec2.push(i);
     }
@@ -47,7 +46,7 @@ fn main() {
     process_using_multithreads( &mut myvec1 );
     println!("vector after processing:");
     printvec(&myvec1);
-    println!("{} elements, elapsed time {:?}",myvec1.len(),starttime.elapsed());
+    println!("{} elements x 1000, elapsed time {:?}",myvec1.len(),starttime.elapsed());
     println!("");
 
     let starttime = Instant::now();
@@ -57,5 +56,5 @@ fn main() {
     process_using_singlethread( &mut myvec2 );
     println!("vector after processing:");
     printvec(&myvec2);
-    println!("{} elements, elapsed time {:?}",myvec2.len(),starttime.elapsed());
+    println!("{} elements x 1000, elapsed time {:?}",myvec2.len(),starttime.elapsed());
 }
